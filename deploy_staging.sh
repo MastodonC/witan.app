85ab69cb1a4be1c81c18b7461936344841035fce
Elise Huard <elise@jabberwocky.eu>
2015-09-02 17:13:48 +0200

    dockerizing and deployment on circle

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,29 +1,9 @@
 #!/usr/bin/env bash
 
-function assert_git_is_clean() {
-    status=$(git status --porcelain)
-
-    if [ -n "${status}" ]; then
-    echo "Working directory is not clean. Aborting..."
-    exit 1
-    fi
-}
-
-assert_git_is_clean
-
 # compile
 lein ring uberjar
 
-# dockerize
-TAG=git-$(git rev-parse --short=12 HEAD)
-IMAGE_NAME=mastodonc/witan.app
-
-docker build -t "${IMAGE_NAME}" .
-docker tag -f "${IMAGE_NAME}" "${IMAGE_NAME}:latest" && \
-docker tag -f "${IMAGE_NAME}" "${IMAGE_NAME}:${TAG}" &&
-docker push "${IMAGE_NAME}"
-
 # using deployment service sebastopol
-TAG=git-$(git rev-parse --short=12 HEAD)
+TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
 sed "s/@@TAG@@/$TAG/" witan.app.json.template > witan.app.json
 curl -i -X POST http://***REMOVED***:9501/marathon/witan.app -H "Content-Type: application/json" -H "***REMOVED***" --data-binary "@witan.app.json"
