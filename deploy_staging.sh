a2324f233bd528174d6e863452b4298f43da4738
Elise Huard <elise@jabberwocky.eu>
2015-09-03 12:11:52 +0200

    header fix

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -3,4 +3,4 @@
 # using deployment service sebastopol
 TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
 sed "s/@@TAG@@/$TAG/" witan.app.json.template > witan.app.json
-curl -i -X POST http://52.28.251.86:9501/marathon/witan.app -H "Content-Type: application/json" -H "x-kixi-keep-it-secret-keep-it-safe" --data-binary "@witan.app.json"
+curl -i -X POST http://52.28.251.86:9501/marathon/witan.app -H "Content-Type: application/json" -H "x-kixi-keep-it-secret-keep-it-safe: null" --data-binary "@witan.app.json"
