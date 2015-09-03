14fa794663e80dd52f1abfcf23673cfc69237792
Antony Woods <acron1@gmail.com>
2015-09-03 20:35:03 +0100

    Added CORS support

--- project.clj
+++ project.clj
@@ -7,13 +7,14 @@
                  [ring/ring-defaults "0.1.2"]
                  [ring/ring-json "0.4.0" :exclusions [ring/ring-core]]
                  [ring/ring-jetty-adapter "1.3.2"]
+                 [ring-cors "0.1.7"]
                  [liberator "0.13"]
                  [cheshire "5.5.0"]
                  [buddy/buddy-auth "0.6.2"]
                  [org.clojure/tools.logging "0.3.1"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler witan.app.handler/app
-         :nrepl {:start? true :port 7888}}
+         :nrepl {:start? true :port 7889}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
