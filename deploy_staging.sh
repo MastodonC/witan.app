d52dd417a7c41a62bd79cefb62635bbda8164e6f
Elise Huard <elise@jabberwocky.eu>
2015-09-03 09:48:30 +0200

    circle deployment

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# compile
-lein ring uberjar
-
 # using deployment service sebastopol
 TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
 sed "s/@@TAG@@/$TAG/" witan.app.json.template > witan.app.json
