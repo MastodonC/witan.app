1277b0e4d7142a35249b18d3c8c6083b7bc53cda
Elise Huard <elise@jabberwocky.eu>
2015-09-04 10:04:56 +0200

    back to ‘.’ for witan.app

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -3,4 +3,4 @@
 # using deployment service sebastopol
 TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
 sed "s/@@TAG@@/$TAG/" witan.app.json.template > witan.app.json
-curl -i -X POST http://***REMOVED***:9501/marathon/witan_app -H "Content-Type: application/json" -H "***REMOVED***: 123" --data-binary "@witan.app.json"
+curl -i -X POST http://***REMOVED***:9501/marathon/witan.app -H "Content-Type: application/json" -H "***REMOVED***: 123" --data-binary "@witan.app.json"
