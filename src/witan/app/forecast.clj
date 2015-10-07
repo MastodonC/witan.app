4dc387ffc5cef332ebce67b02d78fecd6b780caf
Antony Woods <antony@mastodonc.com>
2015-10-07 11:02:01 +0100

    `update-forecast!` added for forecast versions

--- src/witan/app/forecast.clj
+++ src/witan/app/forecast.clj
@@ -30,28 +30,70 @@
              :series-id series_id
              :created (java-Date-to-ISO-Date created))))
 
+(defn find-forecast-by-id
+  [id]
+  (hayt/select :forecast_headers (hayt/where {:id id})))
+
 (defn find-forecast-by-name
   [name]
   (hayt/select :forecast_headers (hayt/where {:name name})))
 
+(defn find-forecasts-by-series-id
+  [series-id]
+  (hayt/select :forecast_headers (hayt/where {:series_id series-id})))
+
+(defn update-forecast-descendant-id
+  [update-id descendant-id]
+  (hayt/update :forecast_headers
+               (hayt/set-columns {:descendant_id descendant-id})
+               (hayt/where {:id update-id})))
+
+(defn retrieve-forecast-most-recent-of-series
+  [series-id]
+  (->> series-id
+       find-forecasts-by-series-id
+       c/exec
+       (sort-by :version >)
+       first))
+
 (defn create-forecast
-  [{:keys [name description owner]}]
+  [{:keys [name description owner series_id version in_progress id]
+    :or {series_id (uuid/random)
+         version 0
+         in_progress false
+         id (uuid/random)}}]
   (hayt/insert :forecast_headers (hayt/values
-                           :id    (uuid/random)
-                           :name  name
-                           :description description
-                           :created (tf/unparse (tf/formatters :date-time) (t/now))
-                           :owner owner ;; TODO check owner exists?
-                           :series_id    (uuid/random)
-                           :version 0
-                           :in_progress false
-                           :descendant_id nil)))
+                                  :id id
+                                  :name  name
+                                  :description description
+                                  :created (tf/unparse (tf/formatters :date-time) (t/now))
+                                  :owner owner ;; TODO check owner exists?
+                                  :series_id    series_id
+                                  :version version
+                                  :in_progress in_progress
+                                  :descendant_id nil)))
 
 (defn add-forecast!
   [{:keys [name] :as forecast}]
-  (let [existing-forecasts (c/exec (find-forecast-by-name name))]
+  (let [existing-forecasts (c/exec (find-forecast-by-name name))
+        id (uuid/random)]
     (when (empty? existing-forecasts)
-      (c/exec (create-forecast forecast)))))
+      (c/exec (create-forecast (assoc forecast :id id)))
+      (first (c/exec (find-forecast-by-id id))))))
+
+(defn update-forecast!
+  [{:keys [series-id owner inputs]}]
+  (if-let [latest-forecast (retrieve-forecast-most-recent-of-series series-id)]
+    (let [new-version (+ (:version latest-forecast) 1)
+          new-id (uuid/random)
+          new-forecast (assoc latest-forecast
+                              :version new-version
+                              :owner owner
+                              :in_progress true
+                              :id new-id)]
+      (c/exec (create-forecast new-forecast))
+      (c/exec (update-forecast-descendant-id (:id latest-forecast) new-id))
+      (first (c/exec (find-forecast-by-id new-id))))))
 
 (defn get-forecasts
   []
--- src/witan/app/test_data.clj
+++ src/witan/app/test_data.clj
@@ -6,14 +6,16 @@
 (defn load-test-data
   "Add a bunch of test data into Cassandra"
   []
+  (let [;; add users
+        user1 (user/add-user! {:name "Mastodon 1" :username "support@mastodonc.com" :password "secret"})
+        user2 (user/add-user! {:name "Mastodon 2" :username "support2@mastodonc.com" :password "secret"})
 
-  ;; add users
-  (user/add-user! {:name "Mastodon 1" :username "support@mastodonc.com" :password "secret"})
-  (user/add-user! {:name "Mastodon 2" :username "support2@mastodonc.com" :password "secret"})
-  (let [id1 (:id (first (c/exec (user/find-user-by-username "support@mastodonc.com"))))
-        id2 (:id (first (c/exec (user/find-user-by-username "support2@mastodonc.com"))))]
+        ;; add a few forecasts
+        f1 (forecast/add-forecast! {:name "My Forecast 1" :description "Description of my forecast" :owner (:id user1)})
+        f2 (forecast/add-forecast! {:name "My Forecast 2" :description "Description of my forecast" :owner (:id user2)})
+        f3 (forecast/add-forecast! {:name "My Forecast 3" :description "Description of my forecast" :owner (:id user1)})
 
-    ;; add a few forecasts
-    (forecast/add-forecast! {:name "My Forecast 1" :description "Description of my forecast" :owner id1})
-    (forecast/add-forecast! {:name "My Forecast 2" :description "Description of my forecast" :owner id2})
-    (forecast/add-forecast! {:name "My Forecast 3" :description "Description of my forecast" :owner id1})))
+        ;; versions of these forecasts
+        f1_1 (forecast/update-forecast! {:series-id (:series_id f1) :owner (:id user1)})
+        f1_2 (forecast/update-forecast! {:series-id (:series_id f1) :owner (:id user1)})
+        f3_1 (forecast/update-forecast! {:series-id (:series_id f3) :owner (:id user1)})]))
