8dc284f69b4542b651c04ddf1bb16040bfbe5eb9
Elise Huard <elise@jabberwocky.eu>
2015-10-19 12:13:46 +0100

    first addition of data in context of model

---
+++ src/witan/app/data.clj
@@ -0,0 +1,29 @@
+(ns witan.app.data
+  (:require [qbits.hayt :as hayt]
+            [qbits.alia.uuid :as uuid]
+            [clj-time.core :as t]
+            [clj-time.format :as tf]
+            [witan.app.config :as c]))
+
+(defn create-data
+  [{:keys [data-id name publisher version s3-url model-id]}]
+  (let [creation-time (tf/unparse (tf/formatters :date-time) (t/now))]
+    (hayt/insert :data (hayt/values :data_id data-id
+                                    :name name
+                                    :model_id model-id
+                                    :publisher publisher
+                                    :version version
+                                    :s3_url s3-url
+                                    :created creation-time))))
+
+(defn add-data!
+  "TODO: for now manually upload and provide s3 url, but need to upload as part of this process"
+  [{:keys [data-id name model-id publisher version s3-url]
+    :or {data-id (uuid/random)
+         version 1}}]
+  (c/exec (create-data {:data-id data-id
+                        :name name
+                        :publisher publisher
+                        :version version
+                        :s3-url s3-url
+                        :model-id model-id})))
--- src/witan/app/test_data.clj
+++ src/witan/app/test_data.clj
@@ -2,6 +2,7 @@
   (:require [witan.app.user :as user]
             [witan.app.forecast :as forecast]
             [witan.app.model :as model]
+            [witan.app.data :as data]
             [witan.app.config :as c]))
 
 (defn load-test-data!
@@ -10,13 +11,31 @@
   (let [;; add users
         user1 (user/add-user! {:name "Mastodon 1" :username "support@mastodonc.com" :password "secret"})
         user2 (user/add-user! {:name "Mastodon 2" :username "support2@mastodonc.com" :password "secret"})
-
         ;; add models
-        m1 (model/add-model! {:name "My Model 1" :description "Description of my model" :owner (:id user1)})
-        m2 (model/add-model! {:name "My Model 2" :description "Description of my model" :owner (:id user2)
-                              :properties [{:name "Some field" :type "text" :context "Placeholder value 123"}]})
-        m3 (model/add-model! {:name "My Model 3" :description "Model with enum" :owner (:id user2)
-                              :properties [{:name "Boroughs" :type "dropdown" :context "Choose a borough" :enum_values ["Camden" "Richmond Upon Thames" "Hackney" "Barnet"]}]})
+        m1 (model/add-model! {:name "My Model 1"
+                              :description "Description of my model"
+                              :owner (:id user1)
+                              :input-data [{:name "Base population data"}]})
+        m2 (model/add-model! {:name "My Model 2"
+                              :description "Description of my model"
+                              :owner (:id user2)
+                              :properties [{:name "Some field" :type "text" :context "Placeholder value 123"}]
+                              :input-data [{:name "Base population data"}]})
+        m3 (model/add-model! {:name "My Model 3"
+                              :description "Model with enum"
+                              :owner (:id user2)
+                              :properties [{:name "Boroughs" :type "dropdown" :context "Choose a borough" :enum_values ["Camden" "Richmond Upon Thames" "Hackney" "Barnet"]}]
+                              :input-data [{:name "SHLAA"}
+                                           {:name "Base population data"}]
+                              :output-data [{:name "housing-linked population"}]})
+        ;; add data
+        d1 (data/add-data! {:name "Base population"
+                            :publisher (:id user1)
+                            :s3-url "https://s3.eu-central-1.amazonaws.com/witan-test-data/Long+Pop.csv"
+                            :model-id (:model_id m1)})
+
+        ;; update model to have this as default data
+
 
         ;; versions of these models
         ;;m1_2 (model/update-model! {:model-id (:model_id m1) :owner (:id user1)})
