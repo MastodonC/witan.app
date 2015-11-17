349c94891586f46ced74c7f4fbaa374f701bbc50
Neale Swinnerton <neale@sw1nn.com>
2015-11-17 10:43:48 +0000

    Add onerous warnings.

--- dev/user.clj
+++ dev/user.clj
@@ -1,4 +1,10 @@
 (ns user
+  
+  ;; DO NOT ADD ANYTHING HERE THAT MIGHT REMOTELY HAVE A COMPILATION ERROR.
+   ;; THIS IS TO ENSURE WE CAN ALWAYS GET A REPL STARTED.
+   ;;
+   ;; see (init) below.
+
   (:require [com.stuartsierra.component :as component]
             [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [witan.application]
