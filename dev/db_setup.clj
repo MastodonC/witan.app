e31687dd4d13f4c7873647100aaa4de924ebbc6b
Elise Huard <elise@jabberwocky.eu>
2015-12-07 21:55:07 +0000

    db setup: drop doesn’t always work

--- dev/db_setup.clj
+++ dev/db_setup.clj
@@ -26,10 +26,12 @@
         db-scripts (clojure.string/split (slurp (io/file (io/resource "db-schema.cql"))) #";")
         replication (get-in config [:cassandra-session :replication])]
     (log/warn "Dropping keyspace " keyspace)
-    (alia/execute session (str "DROP KEYSPACE " keyspace ";"))
+    (try
+      (alia/execute session (str "DROP KEYSPACE " keyspace ";"))
+      (catch Exception e (str "keyspace could not be dropped " (.getMessage e))))
     (log/warn "Recreating keyspace " keyspace)
     (alia/execute session (str "CREATE KEYSPACE " keyspace " WITH replication = " (replication-strategy replication) " AND durable_writes = true;"))
     (log/warn "Loading schema." )
     (alia/execute session (str "USE " keyspace ";"))
-    (run! (fn [scr] (log/info scr) (when-not (clojure.string/blank? scr) (alia/execute session (str scr ";")))) db-scripts)
+    (run! (fn [scr] (log/debug scr) (when-not (clojure.string/blank? scr) (alia/execute session (str scr ";")))) db-scripts)
     (log/warn "Schema loaded.")))
