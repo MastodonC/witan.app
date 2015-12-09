d40162cebaa3bc9fff66da6f1835c6eeedd425d5
Elise Huard <elise@jabberwocky.eu>
2015-12-09 12:17:25 +0000

    now schema is in resources/db-schema.cql

--- cql/generate-staging-schema.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "WARNING: assumption that the actual schema definition only starts on line 6 of cql/dev-schema.cql"
-
-echo "DROP KEYSPACE witan;" > cql/staging-schema.cql
-echo "CREATE KEYSPACE witan WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}  AND durable_writes = true;" >> cql/staging-schema.cql
-sed -n '6,$p' cql/dev-schema.cql >> cql/staging-schema.cql
-echo "Staging schema generated."
