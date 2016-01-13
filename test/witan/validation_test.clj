b4a64c6e0ca5ba33ec816d15d6119a5bb9f10f90
Elise Huard <elise@jabberwocky.eu>
2016-01-13 18:06:12 +0000

    test fixed

--- test/witan/validation_test.clj
+++ test/witan/validation_test.clj
@@ -17,9 +17,9 @@
   (testing "header validation failed"
     (let [validated (validate-content "development-data" (io/file "test-data/bad-header.csv"))]
       (is (not (first validated)))
-      (is (= "The header row of the file is missing one or more required rows. We expect GSS Code Borough,Borough Name,Ward Name,GSS Code Ward" (validation-error validated)))))
+      (is (= "The header row of the file is missing one or more required rows. We expect ward.name,gss.code.ward,gss.code.borough" (validation-error validated)))))
 
   (testing "validation ok"
-    (let [validated (validate-content "development-data" (io/file "test-data/development.csv"))]
+    (let [validated (validate-content "development-data" (io/file "test-data/Template_DevelopmentData_Bexley_WithData.csv"))]
       (is (first validated))
       (is (nil? (second validated))))))
