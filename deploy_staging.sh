29ea6d005d4cce4cbb3a8c0711c847ae97bc5018
Elise Huard <elise@jabberwocky.eu>
2016-01-26 16:09:37 +0000

    continuous deployment on staging

---
+++ deploy_staging.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# using deployment service sebastopol
+TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
+sed "s/@@TAG@@/$TAG/" witan-app.json.template > witan-app.json
+
+# we want curl to output something we can use to indicate success/failure
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST http://$DEPLOY_STAGING_IP:9501/marathon/witan-app -H "Content-Type: application/json" -H "$SEKRIT_HEADER: 123" --data-binary "@witan-app.json")
+echo "HTTP code " $STATUS
+if [ $STATUS == "201" ]
+then exit 0
+else exit 1
+fi
