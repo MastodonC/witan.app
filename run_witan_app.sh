1c47ae24b0b66014076492b4bca481623d46e186
Elise Huard <elise@jabberwocky.eu>
2016-01-28 15:02:08 +0000

    better error msg if ENVIRONMENT not defined

--- run_witan_app.sh
+++ run_witan_app.sh
@@ -5,7 +5,7 @@ cat <<EOF  > /root/.witan-app.edn
 {:cassandra-session {:host "cassandra-dcos-node.cassandra.dcos.mesos"
                      :keyspace "witan"
                      :replication 3}
- :s3 {:bucket "witan-${ENVIRONMENT}-data"}}
+ :s3 {:bucket "witan-${ENVIRONMENT?not defined}-data"}}
 EOF
 
 # running jar
