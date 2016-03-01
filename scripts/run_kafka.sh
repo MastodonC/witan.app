e518f0531ee7bbac7091b5d1cfe3b3e6fd94ffc3
Antony Woods <antony@mastodonc.com>
2016-03-01 10:53:09 +0000

    Update the models to 0.1.7 (also script changes)

--- scripts/run_kafka.sh
+++ scripts/run_kafka.sh
@@ -4,7 +4,7 @@ docker rm zookeeper
 docker rm kafka
 docker run -d --name zookeeper -p 2181:2181 jplock/zookeeper:3.4.6
 docker run -d --name kafka --link zookeeper:zookeeper -p 9092:9092 ches/kafka
-echo "Waiting 5s..."
-sleep 5s
+echo "Waiting 7s..."
+sleep 7s
 docker run --net=host --rm ches/kafka kafka-topics.sh --create --topic test --replication-factor 1 --partitions 1 --zookeeper localhost
 docker run --net=host --rm ches/kafka kafka-topics.sh --create --topic command --replication-factor 1 --partitions 1 --zookeeper localhost
